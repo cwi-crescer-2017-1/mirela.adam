dacb1a8b4814fb6dbc81b8a4c1b451f14588e39b
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-04-13 18:47:46 -0300

    feat(classes CDZ): criar classes armadura, categoria, genero, saint que interagem entre si

--- Modulo1-CDZ/Armadura.java
+++ Modulo1-CDZ/Armadura.java
@@ -1,7 +1,9 @@
 public class Armadura {
    private String constelacao;
+   private Categoria categoria;
    
-   public Armadura(String constelacao){
+   public Armadura(String constelacao, Categoria categoria){
        this.constelacao = constelacao;
+       this.categoria = categoria;
    }
 }

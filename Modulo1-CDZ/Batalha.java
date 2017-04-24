5612bb0159ab241ffe4967a12d53fc35944abfc3
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-04-24 17:06:38 -0300

    refactor(Batalha): melhorar lógica do método iniciar..

--- Modulo1-CDZ/Batalha.java
+++ Modulo1-CDZ/Batalha.java
@@ -1,8 +1,6 @@
-public class Batalha
-{
+public class Batalha {
     private Saint saint1;
     private Saint saint2;
-    // private double dano = 10;
 
     public Batalha(Saint saint1, Saint saint2) {
         this.saint1 = saint1;
@@ -10,9 +8,8 @@ public class Batalha
     }
 
     public void iniciar(){
-        double dano = 10;
         Saint estaAtacando;
-        boolean estaoVivos=true;
+        boolean nenhumMorto=true;
 
         if (saint1.getCategoria() >= saint2.getCategoria()) {
             estaAtacando = this.saint1;
@@ -20,26 +17,12 @@ public class Batalha
             estaAtacando = this.saint2;
         }
         
-        while(estaoVivos){
-            if(estaAtacando.equals(saint1)){
-                this.saint1.getProximoMovimento().executar();
-
-                if(saint2.getVida()==0){
-                    break;
-                } else {
-                    estaAtacando = saint2;
-                }
-
-            } else {
-                this.saint2.getProximoMovimento().executar();
-                if(saint1.getVida()==0){
-                    break;
-                } else {
-                    estaAtacando = saint1;
-                }
-            }
-            
-            estaoVivos = !(saint1.getStatus().equals(Status.MORTO)) || !(saint2.getStatus().equals(Status.MORTO));
+        while(nenhumMorto){
+			//pegando proximo movimento do saint que esta atacando
+            Movimento proximoMovimento = estaAtacando.getProximoMovimento();
+			proximoMovimento.executar();            
+			estaAtacando = estaAtacando == this.saint1 ? this.saint2 : saint1;
+            nenhumMorto = this.saint1.getStatus() != Status.MORTO && this.saint2.getStatus() != Status.MORTO;
         }
     }
 }

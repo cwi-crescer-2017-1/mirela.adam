73c862fa80b4a1029c55786d0a506b512cda784d
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-04-26 16:59:17 -0300

    feat(Sorteador): adicionar interface sorteador, implementação D6 e sortedodia.

---
+++ Modulo1-CDZ/SorteDoDia.java
@@ -0,0 +1,11 @@
+public class SorteDoDia {
+    private Sorteador sorteador;
+    
+    public SorteDoDia(Sorteador sorteador){
+        this.sorteador = sorteador;
+    }
+    public boolean estouComSorte(){
+        int resultado = this.sorteador.sortear();
+        return resultado % 2 == 0;
+    }
+} 

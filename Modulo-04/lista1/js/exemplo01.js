fe93ad244c61df820c586b71065d73153de3d531
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-05-12 17:37:35 -0300

    refactor(modulo04): atualizar solução exercicios.

--- Modulo-04/lista1/js/exemplo01.js
+++ Modulo-04/lista1/js/exemplo01.js
@@ -1,30 +1,26 @@
 <!-- ex1 -->
-function daisyGame(n) {  
-  if(n % 2 === 0) {
-  	return "Love me not"
-  } else {
-  	return "Love me"
-  }
-} 
+function daisyGame(numero) {
+  return numero % 2 !== 0 ? 'Love me' : 'Love me not';
+}
 
 <!-- ex2 -->
 function maiorTexto(texto) {
- var maior = texto[0];
- for (var i = 1; i < texto.length; i++) { 
+ var maior = texto.length > 0 ? texto[0] : "";
+ for (let i = 1; i < texto.length; i++) {
    if (texto[i].length > maior.length){
    	maior = texto[i];
    }
- } 
+ }
  return maior;
 }
 
 <!-- ex3 -->
 function imprime(arrayStrings,funcao){
-	
+
 	if(typeof funcao === 'function'){
-		for (var instrutor of arrayStrings) { 
-  			funcao(instrutor); 
-		} 
+    for (var instrutor of arrayStrings) {
+  			funcao(instrutor);
+		}
 	} else {
 		console.log("Chamada incorreta, ao invés de uma função foi passado um(a): ", typeof funcao);
 	}
@@ -34,6 +30,19 @@ function instrutor(nome){
 	console.log("Olá querido instrutor: ", nome);
 }
 
+//outra solução
+function imprime(textos, funcaoParaExecutar) {
+  textos.forEach(funcaoParaExecutar);
+}
+
+imprime (
+  ['bernardo','nunes','php','zanatta','fabricio','jotz','carlos'],
+  function(instrutor){
+    console.log('ola querido instrutor: ', instrutor);
+  }
+);
+
+
 <!-- ex4 -->
 function somarA(a){
 	function somarB(b){
@@ -42,10 +51,17 @@ function somarA(a){
 	return somarB;
 }
 
+//outra solucao
+function adicionar(numero1){
+    return function(numero2) {
+      return numero1 + numero2;
+    }
+}
+
 
 <!-- ex5 -->
 function fiboSum(n){
-	var soma =0; 
+	var soma =0;
 	while(n > 0){
 		soma += (fibonacci(n));
 		n--;
@@ -62,46 +78,25 @@ function fibonacci (a){
 	}
 }
 
-<!-- ex6 --> 
+<!-- ex6 -->
 function queroCafe(mascada, precos){
-	var precosAbaixo = [0];
-	var i = 0;
+	var precosAbaixo = [];
 
-	for (var valor of precos) { 
+	for (var valor of precos) {
   		 if(valor <= mascada){
-  		 	precosAbaixo[i++] = valor
+  		 	precosAbaixo.push(valor);
   		 }
 	}
 
-	for(var cont = 0; cont < precosAbaixo.length; cont++){
-		if(precosAbaixo[cont] > precosAbaixo[cont+1]){
-			var auxiliar = precosAbaixo[cont];
-			precosAbaixo[cont] = precosAbaixo[cont+1];
-			precosAbaixo[cont+1] = auxiliar;  
+	for(var i = 0; i < precosAbaixo.length; i++){
+		if(precosAbaixo[i] > precosAbaixo[i+1]){
+			var auxiliar = precosAbaixo[i];
+			precosAbaixo[i] = precosAbaixo[i+1];
+			precosAbaixo[i+1] = auxiliar;
 		}
 	}
 
 
 	return precosAbaixo.toString();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//v.sort

3a291ba857ea2921bdd149dd372369477bfdaa5b
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-05-13 20:23:05 -0300

    feat(modulo04): implementar função para séries inválidas - ex1.

---
+++ Modulo-04/lista2/js/exercicios-array.js
@@ -0,0 +1,33 @@
+<!-- ex1 -->
+function seriesInvalidas(series) {
+	let anoAtual = (new Date()).getFullYear();
+	let arrayDeSeriesInvalidas = [];
+	
+	for (let s of series){
+		let invalida = false;
+		if( s.anoEstreia > anoAtual ) {
+			invalida = true;
+		}
+		
+		for (let campo in s) {
+			let valor = s[campo];
+			if(typeof valor === "undefined" || valor === null) {
+				invalida = true;
+			}
+		}
+		
+		if(invalida === true) {
+			arrayDeSeriesInvalidas.push(s.titulo);	
+		} 
+	}
+	return "Séries inválidas: " + (arrayDeSeriesInvalidas).join(" - ");
+}
+
+
+<!-- ex2 -->
+<!-- ex3 -->
+<!-- ex4 -->
+<!-- ex5 -->
+<!-- ex6 --> 
+<!-- ex7 --> 
+<!-- ex8 --> 

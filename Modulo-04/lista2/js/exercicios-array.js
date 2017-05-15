5e0c8355d87a5b6edb889377c5686e40be9553ce
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-05-15 17:14:47 -0300

    refactor(modulo04): melhorar implementação exercicios lista 2.

--- Modulo-04/lista2/js/exercicios-array.js
+++ Modulo-04/lista2/js/exercicios-array.js
@@ -23,18 +23,40 @@ function seriesInvalidas(series) {
 	return "Séries inválidas: " + (arrayDeSeriesInvalidas).join(" - ");
 }
 
+//outra solucao
+function seriesInvalidas1(series){
+	function serieInvalida1(serie) {
+		let algumCampoInvalido = Object.values(serie).some(v => v === null || typeof v === 'undefined');
+		let anoEstreiaInvalido = serie.anoEstreia > new Date().getFullYear();
+		return anoEstreiaInvalido || algumCampoInvalido;
+	}
+	let invalidas = series.filter(serieInvalida1);
+	return `Séries inválidas: ${invalidas.map(s => s.titulo).join(" - ") }`;
+}
 
 //ex2
 function filtrarSeriesPorAno(series, ano) {
-    return series.filter( (s) => s.anoEstreia >= ano );
+    return series.filter( s => s.anoEstreia >= ano );
 }
 
 //ex3
 function mediaDeEpisodios(series) {
 	let quantidadeEpisodios = 0;
-	series.forEach( (serie) => quantidadeEpisodios += serie.numeroEpisodios );
+	series.forEach( serie => quantidadeEpisodios += serie.numeroEpisodios );
 	return quantidadeEpisodios / series.length;
 }
+
+//outras soluções
+function mediaDeEpisodios1(series){
+	return series.reduce( (a,b) => a + b.numeroEpisodios, 0 ) / series.length;
+}
+
+function mediaDeEpisodios2(series){
+	return series
+		.map(e => e.numeroEpisodios)
+		.reduce( (a,b) => a + b ) / series.length;
+}
+
 
 // ex4
 function procurarPorNome(series, nome){
@@ -47,6 +69,11 @@ function procurarPorNome(series, nome){
 	}
 	return false;
 }
+//outra soluçao
+function procurarPorNome1 (series, nome) {
+	return series.some( s => s.elenco.some(e => e.includes(nome) ));
+}
+
 
 //ex5
 function mascadaEmSerie(serie){
@@ -66,6 +93,13 @@ function queroGenero(genero){
 	return lista;
 }
 
+//outra solução
+function queroGenero1 (genero) {
+	return series
+		.filter (s => s.genero.includes(genero))
+		.map(m => m.titulo);
+}
+
 function queroTitulo(texto) {
 	let lista = [];
 	series.forEach((s) => {
@@ -73,6 +107,12 @@ function queroTitulo(texto) {
 			lista.push(s.titulo); }
 	})
 	return lista;
+}
+//outra solução
+function queroTitulo1 (titulo){
+	return series
+		.filter(s => s.titulo.toLowerCase().includes(titulo.toLowerCase()))
+		.map(m => m.titulo);
 }
 
 //ex7
@@ -98,6 +138,34 @@ function creditosIlluminatis(serie) {
     Elenco:\n${serie.elenco.sort(ordenaPorSobrenome).join(', ')}`
   );
 }
+
+//outra solucao
+function creditosIlluminatis1(serie){
+	let criterioOrdenacao = (s1,s2) => {
+		let obterUltimoNome = (s) => {
+			let partesNome = s.split(" ");
+			return partesNome[partesNome.length - 1];
+		}
+
+		return obterUltimoNome(s1).localeCompare(obterUltimoNome(s2));
+	}
+
+	let diretoresOrdenados = serie.diretor.sort(criterioOrdenacao);
+	let elencoOrdenado = serie.elenco.sort(criterioOrdenacao);
+
+	console.log(serie.titulo);
+	console.log("Diretor(es)");
+	console.log(diretoresOrdenados);
+	console.log("Elenco");
+	console.log(elencoOrdenado);
+}
+
+String.prototype.pegarUltimoNome = function() {
+	let partesNome = this.trim().split(" ");
+	return partesNome[partesNome.length - 1];
+}
+
+
 //ex8
 function easterEgg (series) {
 	let letras = [];
@@ -122,9 +190,17 @@ function easterEgg (series) {
 	for (let l of elencoComAbreviacao) {
     	if ( l[(l.indexOf(" ") + 2)] === "." ) {
       		letras.push(l[l.indexOf(".") - 1]);
-  	}
+  		}
 	}
-
   return console.log( `# ${letras.join('')}` );
+}
 
+//outra solucao
+function easterEgg1(){
+	let elencoSerie = series
+		.find(s => s.elenco.every(e => e.temAbreviacao()))
+		.elenco
+		.map(e => e.match(/ [a-z][.] /gi)[0][1])
+		.join("");
+	return `#${ elencoSerie }`;
 }

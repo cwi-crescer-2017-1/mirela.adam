59eb84932b267fe540ce0708f2bd04fab1b2223b
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-05-17 16:41:07 -0300

    fix(pokesearch): corrigir atualizaçao da página ao buscar por novo pokemon.

--- Modulo-04/dia04/Pokemon/js/pokeapi.js
+++ Modulo-04/dia04/Pokemon/js/pokeapi.js
@@ -4,8 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
 var imgPokemon = document.getElementById('img-pokemon');
 let btnPesquisar = document.getElementById('btnPesquisar');
 var inputNumeroPokemon = document.getElementById('numeroPkm');
-var nomePokemon = document.getElementById('nomePokemon');
-var tiposPkm = document.getElementById('tipos');
 
 btnPesquisar.onclick = function () {
 	fetch('https://pokeapi.co/api/v2/pokemon/'+inputNumeroPokemon.value+'/')
@@ -13,23 +11,25 @@ btnPesquisar.onclick = function () {
   	.then(json => { 
     	let div = document.getElementById('detalhesPokemon'); 
       	let img = document.createElement('img'); 
-      	let h1 = document.createElement('h1');
-      	let h3 = document.createElement('h3');
+      	let nomePokemon = document.createElement('h1');
+      	let numeroPokemon = document.createElement('h3');
       	
       	nomePokemon.innerText = "Nome: " +  json.name;
     	numeroPokemon.innerText = "Número: " + json.id;
 
       	img.src = json.sprites.front_default; 
       	 
-      	div.append(h1);
+      	div.append(nomePokemon);
+		div.append(numeroPokemon);
       	div.append(img);
+		
         let tiposDoPokemon = json.types;
     	
     	for (tipos of tiposDoPokemon){
 	        var elemento = document.createElement('li');  
     	    var textoElemento = document.createTextNode(tipos.type.name);  
         	elemento.appendChild(textoElemento);
-        	tiposPkm.appendChild(elemento);
+        	div.appendChild(elemento);
     	}
     
     	json.stats.forEach (x => {

b1d5de133bcd1337990678fde8bd0097e83d1bb2
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-05-17 10:28:00 -0300

    feat(modulo04): implementar busca por numero do pokemon, exercicio 1.

--- Modulo-04/dia04/Pokemon/js/pokeapi.js
+++ Modulo-04/dia04/Pokemon/js/pokeapi.js
@@ -1,5 +1,36 @@
-documento.addEventListener('DOMContentLoaded', function(){
-  let btnPesquisar = document.getElementById('btnPesquisar');
-  btnPesquisar.onclick ( function () {
-                console.log("clicou..."); })
-}
+
+document.addEventListener('DOMContentLoaded', function() { 
+
+var imgPokemon = document.getElementById('img-pokemon');
+let btnPesquisar = document.getElementById('btnPesquisar');
+var inputNumeroPokemon = document.getElementById('numeroPkm');
+var nomePokemon = document.getElementById('nomePokemon');
+var tiposPkm = document.getElementById('tipos');
+
+btnPesquisar.onclick = function () {
+	fetch('https://pokeapi.co/api/v2/pokemon/'+inputNumeroPokemon.value+'/')
+	.then(response => response.json()) 
+  	.then(json => { 
+    	let div = document.getElementById('detalhesPokemon'); 
+      	let img = document.createElement('img'); 
+      	let h1 = document.createElement('h1');
+      	let h3 = document.createElement('h3');
+      	
+      	nomePokemon.innerText = "Nome: " +  json.name;
+    	numeroPokemon.innerText = "Número: " + json.id;
+
+      	img.src = json.sprites.front_default; 
+      	 
+      	div.append(h1);
+      	div.append(img);
+        let tiposDoPokemon = json.types;
+    	
+    	for (tipos of tiposDoPokemon){
+	        var elemento = document.createElement('li');  
+    	    var textoElemento = document.createTextNode(tipos.type.name);  
+        	elemento.appendChild(textoElemento);
+        	tiposPkm.appendChild(elemento);
+    	}
+    })
+   }
+})    

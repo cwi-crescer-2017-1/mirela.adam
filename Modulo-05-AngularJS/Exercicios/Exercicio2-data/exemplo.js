4c348b4ea78640ae08fe44eded774f9e51e7dde2
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-05-18 17:24:27 -0300

    refactor(AngularJS): incluir ponto e vírgulo no arquivo js.

--- Modulo-05-AngularJS/Exercicios/Exercicio2-data/exemplo.js
+++ Modulo-05-AngularJS/Exercicios/Exercicio2-data/exemplo.js
@@ -6,7 +6,7 @@ myApp.controller('Exercicio', function($scope) {
     
     function formataData(){
         var pattern = '/(\d{2})\/(\d{2})\/(\d{4})/';
-        var replace = '$1.$2.$3'
+        var replace = '$1.$2.$3';
         var dataFormatada = $scope.dataTela.replace(pattern, replace);
         console.log(dataFormatada);
         var dataObjeto =  new Date(dataFormatada); 

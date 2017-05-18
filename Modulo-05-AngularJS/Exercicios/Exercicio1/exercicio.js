500c0727f47fc547c6311ff482232254908f8d88
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-05-18 15:37:43 -0300

    refactor(Ex1): adicionar mais pokemons - exercicio 1.

--- Modulo-05-AngularJS/Exercicios/Exercicio1/exercicio.js
+++ Modulo-05-AngularJS/Exercicios/Exercicio1/exercicio.js
@@ -2,8 +2,15 @@ var myApp = angular.module('Exercicio01', []);
 
 myApp.controller('Exercicio', function($scope) {
   $scope.pokemons = [
-      { nome: ' Poke 1 ', tipo: 'água' },
-      { nome: ' Poke 2 ', tipo: 'fogo' },
-      { nome: ' Poke 3', tipo: 'ar' }
+      { nome: 'Venusaur', tipo: 'Grass' },
+      { nome: 'Charmander', tipo: 'Fire' },
+      { nome: 'Persian', tipo: 'Normal' },
+	  { nome: 'Pikachu', tipo: 'Electric' },
+	  { nome: 'Nidoran-m', tipo: 'Poison' },
+	  { nome: 'Kingler', tipo: 'Water' },
+	  { nome: 'Kakuna', tipo: 'Poison' },
+	  { nome: 'Vileplume', tipo: 'Grass' },
+	  { nome: 'Meowth', tipo: 'Normal' },
+	  { nome: 'Swablu', tipo: 'Flying' }
       ];
 });

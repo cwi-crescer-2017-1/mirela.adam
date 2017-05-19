f09faf7a39111e25f55edaeea545548cb74b9b52
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-05-19 16:01:49 -0300

    feat(AngularJS): exemplo Data.

--- Modulo-05-AngularJS/Exercicios/Exercicio2-data/exemploData.js
+++ Modulo-05-AngularJS/Exercicios/Exercicio2-data/exemploData.js
@@ -5,10 +5,7 @@ myApp.controller('Exercicio', function($scope) {
 
     
     function formataData(){
-        var pattern = '/(\d{2})\/(\d{2})\/(\d{4})/';
-        var replace = '$1.$2.$3';
-        var dataFormatada = $scope.dataTela.replace(pattern, replace);
-        console.log(dataFormatada);
+        let dataFormatada = $scope.dataTela.replace(/(\d{2})\/(\d{2})\/(\d{4})/, '$2.$1.$3');
         var dataObjeto =  new Date(dataFormatada); 
         $scope.dataObjeto = dataObjeto;
     }

9918f99cd49f988eccaf7d2f91f1cfa103c00e83
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-05-19 11:26:05 -0300

    feat(pokesearch): atualizações apresentação.

--- Modulo-04/dia04/Pokemon/js/pokeapi.js
+++ Modulo-04/dia04/Pokemon/js/pokeapi.js
@@ -9,32 +9,35 @@ btnPesquisar.onclick = function () {
 	fetch('https://pokeapi.co/api/v2/pokemon/'+inputNumeroPokemon.value+'/')
 	.then(response => response.json()) 
   	.then(json => { 
-    	let div = document.getElementById('detalhesPokemon'); 
+    	  let div = document.getElementById('detalhesPokemon'); 
       	let img = document.createElement('img'); 
       	let nomePokemon = document.createElement('h1');
       	let numeroPokemon = document.createElement('h3');
-      	
-      	nomePokemon.innerText = "Nome: " +  json.name;
-    	numeroPokemon.innerText = "Número: " + json.id;
+      	let tiposTitulo = document.createElement('h3');
+        let nomePkmJson = json.name;
+
+        nomePokemon.innerText = "Nome: " +  nomePkmJson.replace(nomePkmJson.charAt(0), nomePkmJson.charAt(0).toUpperCase());
+    	  numeroPokemon.innerText = "Número: " + json.id;
+        tiposTitulo.innerText = "Tipos:";
 
       	img.src = json.sprites.front_default; 
-      	 
-      	div.append(nomePokemon);
-		div.append(numeroPokemon);
+       	div.append(nomePokemon);
+		    div.append(numeroPokemon);
       	div.append(img);
-		
+		    div.append(tiposTitulo);
         let tiposDoPokemon = json.types;
     	
     	for (tipos of tiposDoPokemon){
 	        var elemento = document.createElement('li');  
-    	    var textoElemento = document.createTextNode(tipos.type.name);  
+    	    var textoElemento = document.createTextNode(tipos.type.name.replace(tipos.type.name.charAt(0), 
+                                                      tipos.type.name.charAt(0).toUpperCase()));  
         	elemento.appendChild(textoElemento);
         	div.appendChild(elemento);
     	}
     
     	json.stats.forEach (x => {
-            let s = document.createElement("h3");
-            s.innerText = x.stat.name;
+            let s = document.createElement("p");
+            s.innerText = x.stat.name.replace(x.stat.name.charAt(0), x.stat.name.charAt(0).toUpperCase());
             let progress = document.createElement("progress");
             progress.max = 100;
             progress.value = x.base_stat;

76872e49846687c20e56e0f8c3f5fb944a1b84b2
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-05-19 07:23:19 -0300

    feat(Angular): implementar listagem de aula e instrutores ordenados - exercício 1.

---
+++ Modulo-05-AngularJS/Exercicios/Exercicio4-tema dia 02/exemplo3.js
@@ -0,0 +1,63 @@
+var myApp = angular.module('Aula02', []);
+
+myApp.filter('mascada',function(){
+  return function(nome){
+         return nome.replace(/(nunes)/i,'$ $1 $');
+  };
+});
+
+
+myApp.controller('Exemplo', function($scope) {
+$scope.instrutores = instrutores;
+
+
+var instrutores = [{
+    nome: 'Bernardo',
+    aula: [{
+        numero: 1,
+        nome: 'OO'
+      },
+      {
+        numero: 4,
+        nome: 'Javascript'
+      }
+    ]
+  },
+  {
+    nome: 'Nunes',
+    aula: [{
+      numero: 2,
+      nome: 'Banco de Dados I'
+    }]
+  },
+  {
+    nome: 'Pedro (PHP)',
+    aula: [{
+      numero: 3,
+      nome: 'HTML e CSS'
+    }]
+  },
+  {
+    nome: 'Zanatta',
+    aula: [{
+      numero: 5,
+      nome: 'AngularJS'
+    }]
+  }
+];
+
+function insereAulaEProfessorEmArray() {
+  var array = [];
+
+  instrutores.forEach(function (i) {
+      for(aula of i.aula) {
+        array.push( { aula: aula, instrutor: i.nome } )
+      }
+    }
+  )
+  return array;
+}
+
+$scope.aulaInstrutor = insereAulaEProfessorEmArray();
+
+});

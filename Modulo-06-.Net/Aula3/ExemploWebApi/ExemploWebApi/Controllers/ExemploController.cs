c2e57e4e67f7c64abc0c08893caeab928ebe8491
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-05-26 17:39:10 -0300

    feat(modulo06): códigos aula 3.

---
+++ Modulo-06-.Net/Aula3/ExemploWebApi/ExemploWebApi/Controllers/ExemploController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExemploWebApi.Controllers
+{
+    public class ExemploController : ApiController
+    {
+        public List<string> Get()
+        {
+            var herois = new List<string>()
+        {
+            "Goku",
+            "Batman",
+            "Ryu"
+        };
+            return herois;
+        }
+    }
+}
---
+++ Modulo-06-.Net/Aula3/ExemploWebApi/ExemploWebApi/Controllers/HeroisController.cs
@@ -0,0 +1,44 @@
+﻿using ExemploWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExemploWebApi.Controllers
+{
+    public class HeroisController : ApiController
+    {
+        private static List<Heroi> ListaHerois { get; set; } = new List<Heroi>();
+        private static int IdContador { get; set; } = 0;
+        private static object objetoLock = new object();
+
+        public IEnumerable<Heroi> Get(string nome=null, int? id=null)
+        {         
+                return ListaHerois.Where( x =>
+                            (id == null || id == x.Id) &&
+                            (nome == null || x.Nome == nome)
+                       );
+            
+        }
+
+        public IHttpActionResult Post(Heroi heroi)
+        {
+
+            if (heroi == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                lock (objetoLock)
+                {
+                    heroi.Id = ++IdContador;
+                    ListaHerois.Add(heroi);
+                }
+                return Ok(heroi); 
+            }
+        }
+    }
+}

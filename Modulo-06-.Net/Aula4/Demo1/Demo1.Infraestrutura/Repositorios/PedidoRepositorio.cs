472e257013fd2e42b7df018c5101c0143a3e6b04
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-05-29 17:33:33 -0300

    feat(.Net): arquivos aula 4 - 29/05/2017.

---
+++ Modulo-06-.Net/Aula4/Demo1/Demo1.Infraestrutura/Repositorios/PedidoRepositorio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo1.Dominio.Entidades;
+
+namespace Demo1.Infraestrutura.Repositorios
+{
+    class PedidoRepositorio : IPedidoRepositorio
+    {
+        public void Alterar(Pedido pedido)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Criar(Pedido pedido)
+        {
+            //realizar o insert do pedido 
+            //obter o ultimo id do pedido
+            
+            //para cada item do pedido, realizar o insert do ItemPedido
+            //obter ultimo id do ItemPedido
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pedido> Listar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pedido Obter(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Modulo-06-.Net/Aula4/Demo1/Demo1.Infraestrutura/Repositorios/ProdutoRepositorio.cs
@@ -0,0 +1,76 @@
+﻿using Demo1.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1.Infraestrutura.Repositorios
+{
+    public class ProdutoRepositorio
+    {
+        string stringConexao = "Server=13.65.101.67;User Id=mirela.adam;Password=123456;Database=aluno06db";
+
+        public void Criar(Produto produto)
+        {
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = @"INSERT INTO Produto (Nome, Preco, Estoque) 
+                                            VALUES (@nome, @preco, @estoque)";
+
+                    comando.Parameters.AddWithValue("@nome", produto.Nome);
+                    comando.Parameters.AddWithValue("@preco", produto.Preco);
+                    comando.Parameters.AddWithValue("@estoque", produto.Estoque);
+
+                    comando.ExecuteNonQuery(); // Executa o comando e retorna somente a quantidade de linhas afetadas
+                                               /* 
+                                                * comando.ExecuteReader(); // Executa o comando e retorna um SqlDataReader - ler resultado de select 
+                                                * comando.ExecuteScalar(); // Executa o comando e retorna o primeiro resultado 
+                                               */
+                }
+
+                //obtem ultimo ID criado
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = @"SELECT @@IDENTITY";
+                    var result = (decimal)comando.ExecuteScalar();
+                    produto.Id = (int)result;
+                }
+            }
+        }
+
+        public List<Produto> Listar()
+        {
+            var produtos = new List<Produto>();
+
+            using(var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "SELECT Id, Nome, Preco, Estoque FROM Produto";
+                    var dataReader = comando.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        var produto = new Produto();
+
+                        produto.Id = (int)dataReader["Id"];
+                        produto.Nome = (string)dataReader["Nome"];
+                        produto.Preco = (decimal)dataReader["Preco"];
+                        produto.Estoque = (int)dataReader["Estoque"];
+
+                        produtos.Add(produto);
+                    }
+                } 
+            }
+
+
+            return produtos;
+        }
+    }
+}

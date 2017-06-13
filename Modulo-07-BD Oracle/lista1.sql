90fdea223b1a23bcb63c31d631dad102066fee2a
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-06-13 15:13:01 -0300

    feat(modulo07): correção lista 1.

--- Modulo-07-BD Oracle/lista1.sql
+++ Modulo-07-BD Oracle/lista1.sql
@@ -1,35 +1,26 @@
 --EXERCICIOS 
 -- Liste os produtos (id e nome) que não tiveram nenhuma compra nos últimos quatro meses.
+create view vwProdutos_Sem_Compra as
 select p.idproduto, p.nome 
 from produto p 
 where p.idproduto not in (
   select pit.idproduto from pedidoitem pit 
-  inner join pedido ped
-  on ped.idpedido = pit.idpedido
-  where ped.datapedido > add_months(trunc(sysdate), -4)
+  inner join pedido ped on ped.idpedido = pit.idpedido
+  where ped.datapedido >= add_months(trunc(sysdate), -4)
 );
-
 --Altere o status dos produtos (campo situacao) que não tiveram nenhum pedido nos últimos quatro meses.
-
-update produto prod
-set prod.situacao = 'I'
-where prod.idproduto not in (
-  select pit.idproduto from pedidoitem pit 
-  inner join pedido p
-  on p.idpedido = pit.idpedido
-  where p.datapedido > add_months(trunc(sysdate), -4)
-);
-
+update produto
+set situacao = 'I'
+where idproduto in (select IDProduto from vwProdutos_Sem_Compra)
+and situacao='A';
+--ou
+update produto 
+set dituacao = 'I'
+where exists (select IdProduto from vwProdutos_Sem_compra vw where Produto.IdProduto = vw.IdProduto) and Situacao = 'A';
 --Faça uma consulta que receba um parâmetro sendo o IDProduto e liste a quantidade de itens na tabela 
 -- PedidoItem com este IDProduto foram vendidos no último ano (desde janeiro/2017).
-select sum(pit.Quantidade) as quantidade
-from pedidoitem pit
-where pit.IdProduto = :IdProduto
-and pit.IdProduto in (
-  select pit.idproduto 
-  from pedidoitem pite 
-  inner join pedido p
-  on p.idpedido = pite.idpedido
-  where p.datapedido >= to_date('01/01/2017', 'DD/MM/YYYY')
-)
-group by pit.idproduto;
+select sum(item.Quantidade) as quantidade
+from pedidoitem item
+  inner join pedido ped on ped.IDPedido = item.IDPedido
+where item.IdProduto = :p_IdProduto
+and ped.DataPedido >= trunc(sysdate, 'yyyy');

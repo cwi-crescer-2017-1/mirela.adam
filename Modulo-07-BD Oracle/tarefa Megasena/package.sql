31b675e23d0e5ee5abef890daba8cf0c9a02ebc9
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-06-19 12:06:46 -0300

    feat(megasena): exercicio b - incompleto.

--- Modulo-07-BD Oracle/tarefa Megasena/package.sql
+++ Modulo-07-BD Oracle/tarefa Megasena/package.sql
@@ -1,3 +1,27 @@
+create or replace package pck_megasena is
+
+  -- Author  : ANDRENUNES
+  -- Purpose : Manipulação na base de dados da Loteria mais conhecida do Brasil
+  
+  -- Variáveis Globais - definidas em procedimento específico
+  gPremio_sena          number(12,2) := 0;
+  gPremio_quina         number(12,2) := 0;
+  gPremio_quadra        number(12,2) := 0;
+  gAcumulado_proximo_05 number(12,2) := 0;
+  gAcumulado_final_ano  number(12,2) := 0;
+
+  -- Public type declarations
+  procedure salvaConcurso (pConcurso in integer,
+                           pData     in date,
+                           pPremio   in number);
+  procedure geraProximoConcurso;
+  function buscaPercentual(pIdentificador in varchar2) return number;
+  procedure atualizaAcertadores (pConcurso in integer);
+
+end pck_megasena;
+
+----------------------------------------------------
+
 create or replace package body pck_megasena is
 
   /* Busca valores percentuais conforme regra definida na tabela 'Regra_Rateio_Premio' */

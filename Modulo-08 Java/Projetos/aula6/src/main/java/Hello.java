2fd165c9952d3d24e42460cb48ca4aaf3170ec3c
Mirela Haag Adam <mirela.adam@cwi.com.br>
2017-06-27 17:31:54 -0300

    feat(Java): subir arquivos aula 6.

---
+++ Modulo-08 Java/Projetos/aula6/src/main/java/Hello.java
@@ -0,0 +1,18 @@
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class Hello {
+
+    private String helloWorld = "Hello World!";
+
+    public String getHelloWorld() {
+        return helloWorld;
+    }
+
+    public void setHelloWorld(String helloWorld) {
+        this.helloWorld = helloWorld;
+    }
+
+}
